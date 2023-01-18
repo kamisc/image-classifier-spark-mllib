9edefea6eb4abfb5e0553fcf9829b74972ee068b
Kamil Seweryn <kamil_seweryn@o2.pl>
2023-01-18 20:09:14 +0100

    data preprocessing

--- src/main/java/com/sewerynkamil/ImageClassifier.java
+++ src/main/java/com/sewerynkamil/ImageClassifier.java
@@ -1,13 +1,51 @@
 package com.sewerynkamil;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.io.IOException;
+
 public class ImageClassifier {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Image classifier")
                 .getOrCreate();
 
+        preProcessImage();
+
+        Dataset<Row> train = spark.read().format("libsvm").load("data/train.txt");
+        Dataset<Row> test = spark.read().format("libsvm").load("data/test.txt");
+    }
+
+    public static void preProcessImage() throws IOException {
+        ImagePreprocessor trainPositive =  new ImagePreprocessor(
+                "data/train/hotdog",
+                1,
+                "data/train.txt"
+        );
+        trainPositive = null;
+
+        ImagePreprocessor trainNegative =  new ImagePreprocessor(
+                "data/train/nothotdog",
+                0,
+                "data/train.txt"
+        );
+        trainNegative = null;
+
+        ImagePreprocessor testPositive =  new ImagePreprocessor(
+                "data/test/hotdog",
+                1,
+                "data/test.txt"
+        );
+        testPositive = null;
+
+        ImagePreprocessor testNegative =  new ImagePreprocessor(
+                "data/test/nothotdog",
+                0,
+                "data/test.txt"
+        );
+        testNegative = null;
     }
 }
--- src/main/java/com/sewerynkamil/ImagePreprocessor.java
+++ src/main/java/com/sewerynkamil/ImagePreprocessor.java
@@ -1,8 +1,13 @@
 package com.sewerynkamil;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
@@ -19,15 +24,56 @@ public class ImagePreprocessor {
         this.outputFilename = outputFilename;
 
         this.loadImages();
+        this.transformImages();
     }
 
     private void loadImages() throws IOException {
-        Files.walkFileTree(Paths.get(this.imageDirectory), new SimpleFileVisitor<Path>() {
+        Files.walkFileTree(Paths.get(this.imageDirectory), new SimpleFileVisitor<>() {
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 images.add(ImageIO.read(file.toFile()));
                 return FileVisitResult.CONTINUE;
             }
         });
+    }
+
+    private void transformImages() throws IOException {
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(this.outputFilename, true)));
+
+        for (BufferedImage image : images) {
+            // make a grayscale image
+            toGrayScale(image);
+            // write the pixel to a libsvm file
+            Raster raster = image.getData();
+            int width = image.getWidth();
+            int height = image.getHeight();
+            out.print(this.label);
+            int pixelCount = 1;
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    float normalizedPixel = raster.getSample(i, j, 0) / 255.0f;
+                    out.print(" " + pixelCount + ":" + normalizedPixel);
+                    pixelCount++;
+                }
+            }
+            out.print("");
+        }
+        out.close();
+    }
+
+    public static void toGrayScale(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Color c = new Color(image.getRGB(i, j));
+                int r = (int) (c.getRed() * 0.21);
+                int g = (int) (c.getGreen() * 0.72);
+                int b = (int) (c.getBlue() * 0.07);
+                int sum = r + g + b;
+                Color grayPixel = new Color(sum, sum, sum);
+                image.setRGB(i, j, grayPixel.getRGB());
+            }
+        }
     }
 }
